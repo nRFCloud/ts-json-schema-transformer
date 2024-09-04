507c518c6dccdb49236c9be3dfbe8620f59c5483
John Conley <8932043+jfrconley@users.noreply.github.com>
2024-09-04 13:00:24 -0700

    Add default export (#36)

--- src/index.ts
+++ src/index.ts
@@ -211,3 +211,25 @@ type Exact<T, U> = IfEquals<T, U, T, never>;
 type IfEquals<T, U, Y = unknown, N = never> = (<G>() => G extends T ? 1 : 2) extends (<G>() => G extends U ? 1 : 2) ? Y
   : N;
 type NotAny<T> = 0 extends (1 & T) ? never : T;
+
+export default {
+  getSchema,
+  validate,
+  createValidateFn,
+  guard,
+  createGuardFn,
+  createValidator,
+  getMockObject,
+  mock,
+  createMockFn,
+  assertValid,
+  assertGuard,
+  createAssertGuardFn,
+  assert,
+  createAssertFn,
+  parse,
+  createParseFn,
+  assertParse,
+  createAssertParseFn,
+  ValidationError,
+};
